fb0e10f2d79b8af42d548c113dbf51fa583dd2c4
katsiaryna.sebiatsinskaya <katsiaryna.sebiatsinskaya@pandadoc.com>
2025-10-22 15:01:22 +0200

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, HashRouter } from 'react-router';
 
 import { App } from './App';
 import './index.css';
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pet-project">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
